6ced7f9ac9086d1dbf406a4ccdc14065ab3784d3
mattmc3 <mattmc3@gmail.com>
2020-12-24 16:00:57 -0500

    Only ls after cd-ing in an interactive session

--- zsh-cd-ls.plugin.zsh
+++ zsh-cd-ls.plugin.zsh
@@ -1,8 +1,10 @@
-# 'ls' after 'cd'
+# 'ls' after every 'cd'
 if ! (( $chpwd_functions[(I)chpwd_cdls] )); then
   chpwd_functions+=(chpwd_cdls)
 fi
 function chpwd_cdls() {
-  emulate -L zsh
-  \ls -FG
+  if [[ -o interactive ]]; then
+    emulate -L zsh
+    ls
+  fi
 }
