94fdbd2907b2e8e156ff8640e3780ee88571a242
mattmc3 <mattmc3@gmail.com>
2023-04-17 22:38:08 -0400

    Update style

--- cd-ls.plugin.zsh
+++ cd-ls.plugin.zsh
@@ -1,2 +1,10 @@
-0=${(%):-%N}
-source ${0:A:h}/cd-ls.zsh
+# 'ls' after every 'cd'
+if ! (( $chpwd_functions[(I)chpwd_cdls] )); then
+  chpwd_functions+=(chpwd_cdls)
+fi
+function chpwd_cdls() {
+  if [[ -o interactive ]]; then
+    emulate -L zsh
+    ls
+  fi
+}
